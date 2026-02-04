c9786ec1d53cccf8634d070e17eadc98923d07dc
Andy <andyjavierrd@gmail.com>
2026-02-04 19:26:25 -0400

    Crear appModule

---
+++ app/src/main/java/com/example/andy_javier_ap2_p1/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.example.andy_javier_ap2_p1.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.andy_javier_ap2_p1.data.local.database.PrimerParcialDb
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePrimerParcialDb(@ApplicationContext appContext: Context): PrimerParcialDb {
+        return Room.databaseBuilder(
+            appContext,
+            PrimerParcialDb::class.java,
+            "PrimerParcial.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    fun provideBorrameDao(db: PrimerParcialDb) = db.borrameDao()
+}

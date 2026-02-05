1eeff43616892c69dd880bbdf3a0cbce87fcba4f
Andy <andyjavierrd@gmail.com>
2026-02-04 21:32:57 -0400

    editviewmodel

--- app/src/main/java/com/example/andy_javier_ap2_p1/MainActivity.kt
+++ app/src/main/java/com/example/andy_javier_ap2_p1/MainActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.andy_javier_ap2_p1.presentation.navigation.PrimerParcialNavHost
@@ -21,11 +21,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Andy_Javier_AP2_P1Theme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val navController = rememberNavController()
-                    PrimerParcialNavHost(
-                        navController = navController
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        PrimerParcialNavHost(
+                            navController = navController
+                        )
+                    }
                 }
             }
         }

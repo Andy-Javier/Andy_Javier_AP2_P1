034fe17a1b1c5a3fa8f5e22e11100434e440c1e4
Andy <andyjavierrd@gmail.com>
2026-02-11 23:22:05 -0400

    Agregar GetCervezaUseCase

--- app/src/main/java/com/example/andy_javier_ap2_p1/domain/usecase/GetCervezaUseCase.kt
+++ app/src/main/java/com/example/andy_javier_ap2_p1/domain/usecase/GetCervezaUseCase.kt
@@ -1,6 +1,13 @@
 package com.example.andy_javier_ap2_p1.domain.usecase
 
+import com.example.andy_javier_ap2_p1.domain.models.Cerveza
+import com.example.andy_javier_ap2_p1.domain.repository.CervezaRepository
 import javax.inject.Inject
 
-class GetCervezaUseCase @Inject constructor() {
+class GetCervezaUseCase @Inject constructor(
+    private val repository: CervezaRepository
+) {
+    suspend operator fun invoke(id: Int): Cerveza? {
+        return repository.getCerveza(id)
+    }
 }

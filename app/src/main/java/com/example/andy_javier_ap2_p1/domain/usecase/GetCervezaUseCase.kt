13f3277c86020440ae3cd7204a7d47b37efd8383
Andy <andyjavierrd@gmail.com>
2026-02-11 20:10:33 -0400

    cambio en la clase

--- app/src/main/java/com/example/andy_javier_ap2_p1/domain/usecase/GetCervezaUseCase.kt
+++ app/src/main/java/com/example/andy_javier_ap2_p1/domain/usecase/GetCervezaUseCase.kt
@@ -2,5 +2,5 @@ package com.example.andy_javier_ap2_p1.domain.usecase
 
 import javax.inject.Inject
 
-class UpsertPrimerParcialUseCase @Inject constructor() {
+class GetCervezaUseCase @Inject constructor() {
 }

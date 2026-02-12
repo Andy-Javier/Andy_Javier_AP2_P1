cbe753bfd754929c982d6832a601950b4613b30a
Andy <andyjavierrd@gmail.com>
2026-02-11 23:19:42 -0400

    CervezaRepository.kt

--- app/src/main/java/com/example/andy_javier_ap2_p1/domain/repository/CervezaRepository.kt
+++ app/src/main/java/com/example/andy_javier_ap2_p1/domain/repository/CervezaRepository.kt
@@ -1,7 +1,13 @@
 package com.example.andy_javier_ap2_p1.domain.repository
 
-import com.example.andy_javier_ap2_p1.data.local.entities.CervezaEntity
+import com.example.andy_javier_ap2_p1.domain.models.Cerveza
+import kotlinx.coroutines.flow.Flow
 
 interface CervezaRepository {
-    suspend fun upsert(cerveza: CervezaEntity)
+    fun observeCervezas(nombre: String?, marca: String?): Flow<List<Cerveza>>
+    suspend fun getCerveza(id: Int): Cerveza?
+    suspend fun upsert(cerveza: Cerveza): Int
+    suspend fun delete(id: Int)
+    suspend fun getTotal(nombre: String?, marca: String?): Int
+    suspend fun getPromedio(nombre: String?, marca: String?): Double?
 }

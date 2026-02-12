c7e61804573df341ec7fc7ade9c968961e89b9bd
Andy <andyjavierrd@gmail.com>
2026-02-11 23:23:11 -0400

    Agregar ObserveCervezaUseCase

--- app/src/main/java/com/example/andy_javier_ap2_p1/domain/usecase/ObserveCervezaUseCase.kt
+++ app/src/main/java/com/example/andy_javier_ap2_p1/domain/usecase/ObserveCervezaUseCase.kt
@@ -1,6 +1,22 @@
 package com.example.andy_javier_ap2_p1.domain.usecase
 
+import com.example.andy_javier_ap2_p1.domain.models.Cerveza
+import com.example.andy_javier_ap2_p1.domain.repository.CervezaRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class ObserveCervezaUseCase @Inject constructor() {
+class ObserveCervezaUseCase @Inject constructor(
+    private val repository: CervezaRepository
+) {
+    operator fun invoke(nombre: String?, marca: String?): Flow<List<Cerveza>> {
+        return repository.observeCervezas(nombre, marca)
+    }
+
+    suspend fun getTotal(nombre: String?, marca: String?): Int {
+        return repository.getTotal(nombre, marca)
+    }
+
+    suspend fun getPromedio(nombre: String?, marca: String?): Double? {
+        return repository.getPromedio(nombre, marca)
+    }
 }

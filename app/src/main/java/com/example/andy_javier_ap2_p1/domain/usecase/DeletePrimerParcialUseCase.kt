6097f43777a7f3e58b70e14a94e243851cc52895
Andy <andyjavierrd@gmail.com>
2026-02-11 20:05:43 -0400

    modificacion del nombre a ObservePrimerParcialUseCase.kt

--- app/src/main/java/com/example/andy_javier_ap2_p1/domain/usecase/DeletePrimerParcialUseCase.kt
+++
@@ -1,6 +0,0 @@
-package com.example.andy_javier_ap2_p1.domain.usecase
-
-import javax.inject.Inject
-
-class DeletePrimerParcialUseCase @Inject constructor() {
-}
--- app/src/main/java/com/example/andy_javier_ap2_p1/domain/usecase/ObserveCervezaUseCase.kt
+++ app/src/main/java/com/example/andy_javier_ap2_p1/domain/usecase/ObserveCervezaUseCase.kt
@@ -2,5 +2,5 @@ package com.example.andy_javier_ap2_p1.domain.usecase
 
 import javax.inject.Inject
 
-class ObservePrimerParcialUseCase @Inject constructor() {
+class ObserveCervezaUseCase @Inject constructor() {
 }
